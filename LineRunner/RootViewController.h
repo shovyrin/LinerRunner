63345ec6eb59ebc0d31a0c1b6d5eeec7ae53f73c
Yaroslav Shovyrin <leo_y@ukr.net>
2012-03-06 11:54:56 +0200

    Initial Commit

---
+++ LineRunner/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  LineRunner
+//
+//  Created by Apple Air on 06.03.12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
